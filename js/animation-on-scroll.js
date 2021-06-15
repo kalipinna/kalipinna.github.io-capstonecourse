4410d8cc24bcfada6393f19bd832842dbd942a20
kalipinna <eliachaan@hotmail.it>
2021-06-15 15:48:19 +0200

    updated

---
+++ js/animation-on-scroll.js
@@ -0,0 +1,27 @@
+$(window).one('scroll', function() {
+  if ($("#worktitle").is(':visible')) {
+    $("#worktitle").addClass(" zoom-in-left");
+  }
+
+});
+
+$(window).one('scroll', function() {
+  if ($("#previewimg1, #previewimg2").is(':visible')) {
+    $("#previewimg1, #previewimg2").addClass(" zoom-in-center");
+  }
+});
+
+
+$(window).one('scroll', function() {
+  if ($("#previewimg3, #about-title").is(':visible')) {
+    $("#previewimg3, #about-title").addClass(" zoom-in-center");
+  }
+
+});
+
+$(window).one('scroll', function() {
+  if ($("#icons").is(':visible')) {
+    $("#icons").addClass(" zoom-in-icons");
+  }
+
+});
