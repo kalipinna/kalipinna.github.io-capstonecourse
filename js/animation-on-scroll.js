66b968911a06366cdd8c325cbed3cde2d97b1848
kalipinna <eliachaan@hotmail.it>
2021-06-15 21:09:42 +0200

    updates

--- js/animation-on-scroll.js
+++ js/animation-on-scroll.js
@@ -11,7 +11,21 @@ $(window).one('scroll', function() {
   }
 });
 
+$(window).one('scroll', function() {
+  if ($("#contact-title").is(':visible')) {
+    $("#previewimg3, #about-title").addClass(" zoom-in-center");
+  }
 
+});
+
+$(window).one('scroll', function() {
+  if ($("#contact-title").is(':visible')) {
+    $("#icons").addClass(" zoom-in-icons");
+  }
+
+});
+
+/*
 $(window).one('scroll', function() {
   if ($("#previewimg3, #about-title").is(':visible')) {
     $("#previewimg3, #about-title").addClass(" zoom-in-center");
@@ -25,3 +39,4 @@ $(window).one('scroll', function() {
   }
 
 });
+*/
