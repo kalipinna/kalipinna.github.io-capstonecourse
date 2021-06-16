41b2df76bb7dd64f35b84a2e3d32deec669624ce
kalipinna <eliachaan@hotmail.it>
2021-06-16 17:36:56 +0200

    updates

--- js/animationforworks.js
+++ js/animationforworks.js
@@ -1,13 +1,42 @@
-$(window).one('scroll', function() {
-  if ($("#intro").is(':visible')) {
+$(window).on('scroll', function() {
+  if ($("#slideshow-container").is(':visible')) {
     $("#intro").addClass(" scalefade");
   }
 
 });
 
-$(window).one('scroll', function() {
+$(window).on('scroll', function() {
   if ($("#intro").is(':visible')) {
     $("#sentimental").addClass(" fade");
+  }
+
+});
+
+$(window).on('scroll', function() {
+  if ($("#slideshow-container").is(':visible')) {
+    $("#slideshow-container").addClass(" scalefade");
+  }
+
+});
+
+$(window).on('scroll', function() {
+  if ($("#stars").is(':visible')) {
+    $("#butthen").addClass(" grows");
+  }
+
+});
+
+$(window).on('scroll', function() {
+  if ($("#happier").is(':visible')) {
+    $("#happier").addClass("type");
+  }
+
+});
+
+$(window).on('scroll', function() {
+  if ($("#because").is(':visible')) {
+    $("#free").addClass(" colorscale");
+
   }
 
 });
