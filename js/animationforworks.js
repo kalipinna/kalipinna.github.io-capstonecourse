35feecdb61390b14bebdefb3670282144c9b57e2
kalipinna <eliachaan@hotmail.it>
2021-06-17 15:57:19 +0200

    updated 17/06

--- js/animationforworks.js
+++ js/animationforworks.js
@@ -110,3 +110,17 @@ $(window).on('scroll', function() {
     $('#free').addClass(' colorscale');
   }
 });
+
+$(window).on('scroll', function() {
+  var $elem = $('#credits');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
+  if (elemBottom < docViewBottom) {
+
+    $('#contacts').addClass(' contacts');
+  }
+});
