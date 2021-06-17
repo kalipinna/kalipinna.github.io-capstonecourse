74fbd18f546a1bd08133d70ce8481e0bf4a43d36
kalipinna <eliachaan@hotmail.it>
2021-06-17 12:36:02 +0200

    updated on 17/06

--- js/animation-on-scroll.js
+++ js/animation-on-scroll.js
@@ -15,7 +15,7 @@ $(window).on('scroll', function() {
 
 
 $(window).on('scroll', function() {
-  var $elem = $('#worktitle');
+  var $elem = $('#previewimg1');
   var $window = $(window);
 
   var docViewTop = $window.scrollTop();
