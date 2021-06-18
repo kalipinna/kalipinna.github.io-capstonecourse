bd514700a1ddd6380b1e2391ca0b493f3cbda82f
kalipinna <eliachaan@hotmail.it>
2021-06-18 17:05:19 +0200

    updated 18/06

--- js/about.js
+++ js/about.js
@@ -4,4 +4,13 @@ function show(element) {
   a.style.backgroundImage = b;
 
   document.getElementById('description').innerHTML = element.nextElementSibling.innerHTML;
+  document.getElementById('x').style.display = "block";
+}
+
+function back() {
+
+  document.getElementById('previewimg').style.backgroundImage = "url('images/6.jpg')";
+  document.getElementById('description').innerHTML = "if you haven't clicked on anything, here's a nice landscape for you."
+
+
 }
---
+++ js/animation-for-about.js
@@ -0,0 +1,71 @@
+$(window).on('scroll', function() {
+  var $elem = $('#click');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
+  if (elemBottom < docViewBottom) {
+
+    $('#click').addClass(' skewing');
+  }
+});
+
+$(window).on('scroll', function() {
+  var $elem = $('#intro-to-bio');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
+  if (elemBottom < docViewBottom) {
+
+    $('#intro-to-bio, #intro-to-bio2').addClass(' small');
+  }
+});
+
+
+$(window).on('scroll', function() {
+  var $elem = $('#ido');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
+  if (elemBottom < docViewBottom) {
+
+    $('#ido').addClass(' scale3d');
+  }
+});
+
+$(window).on('scroll', function() {
+  var $elem = $('#ido');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
+  if (elemBottom < docViewBottom) {
+
+    $('#ido1, #ido2, #ido3').addClass(' move');
+  }
+});
+
+
+$(window).on('scroll', function() {
+  var $elem = $('#thanks');
+  var $window = $(window);
+
+  var docViewTop = $window.scrollTop();
+  var docViewBottom = docViewTop + $window.height();
+  var elemTop = $elem.offset().top;
+  var elemBottom = elemTop + $elem.height();
+  if (elemBottom < docViewBottom) {
+
+    $('#thanks').addClass(' thanks');
+  }
+});
